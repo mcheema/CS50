a7a1d7d1655a20778d2443b6c86765291684fce7
Munawar Cheema <munawar.a.cheema@gmail.com>
2024-01-16 11:34:46 +0000

    Added the half exercise.

---
+++ half/half.c
@@ -0,0 +1,26 @@
+// Calculate your half of a restaurant bill
+// Data types, operations, type casting, return value
+// CFLAGS=-lcs50 make half
+// check50 cs50/labs/2023/x/half
+
+#include <cs50.h>
+#include <stdio.h>
+
+float half(float bill, float tax, int tip);
+
+int main(void)
+{
+    float bill_amount = get_float("Bill before tax and tip: ");
+    float tax_percent = get_float("Sale Tax Percent: ");
+    int tip_percent = get_int("Tip percent: ");
+
+    printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
+}
+
+float half(float bill, float tax, int tip)
+{
+    bill /= 2.0;
+    bill *= (1. + tax / 100.);
+    bill *= (1. + tip / 100.);
+    return bill;
+}
