130c446186e8c10797e100f7a36d6b0feec838c9
Munawar Cheema <munawar.a.cheema@gmail.com>
2024-01-16 14:03:48 +0000

    Added comments to the code

--- week1/mario-more/mario.c
+++ week1/mario-more/mario.c
@@ -10,9 +10,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-#define MAXHEIGHT 8
-#define MINHEIGHT 1
-#define PYRAMID_CHAR '#'
+#define MAXHEIGHT 8             /* Per specifications */
+#define MINHEIGHT 1             /* Per specifications */
+#define PYRAMID_CHAR '#'        /* Per specifications */
 
 void print_row(int row, int height);
 int main(void)
@@ -30,16 +30,19 @@ int main(void)
     }
 }
 
+/* Print a row number row,  for a pyramid of height height */
 void print_row(int row, int height)
 {
+    /* Print the padding and section before the gap */
     for (int i = 1; i <= height; i++)
     {
         printf("%c", (i > height - row - 1) ? PYRAMID_CHAR : ' ');
     }
-    printf("  ");
+    printf("  ");               /* print the gap */
+    /* print the section after the gap */
     for (int i = 0; i <= row; i++)
     {
         printf("%c", PYRAMID_CHAR);
     }
-    printf("\n");
+    printf("\n");               /* end of row */
 }
